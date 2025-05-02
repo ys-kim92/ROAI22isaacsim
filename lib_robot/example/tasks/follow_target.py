96bbcabed6c900a84dc1940d11c483b3946b5ee9
roai22 <ys.kim@roai.im>
2025-05-02 13:08:02 +0900

    [완료]shared tagrt 생성

--- lib_robot/example/tasks/follow_target.py
+++ lib_robot/example/tasks/follow_target.py
@@ -10,9 +10,15 @@ from typing import Optional
 
 import isaacsim.core.api.tasks as tasks
 import numpy as np
-from isaacsim.core.utils.prims import is_prim_path_valid
-from isaacsim.core.utils.string import find_unique_string_name
 from isaacsim.examples.interactive.lib_robot.example import Franka
+from isaacsim.core.api.objects import VisualCuboid
+
+from isaacsim.core.api.scenes.scene import Scene
+from isaacsim.core.utils.prims import is_prim_path_valid
+from isaacsim.core.utils.rotations import euler_angles_to_quat
+from isaacsim.core.utils.stage import get_stage_units
+from isaacsim.core.utils.string import find_unique_string_name
+
 
 class FollowTarget(tasks.FollowTarget):
     """[summary]
@@ -39,33 +45,44 @@ class FollowTarget(tasks.FollowTarget):
         franka_prim_path: Optional[str] = None,
         franka_robot_name: Optional[str] = None,
     ) -> None:
+        
+        if target_prim_path is not None and target_name is None:
+            target_name = target_prim_path.split("/")[-1]
+        
         tasks.FollowTarget.__init__(
             self,
             name=name,
             target_prim_path=target_prim_path,
             target_name=target_name,
-            target_position=target_position,
-            target_orientation=target_orientation,
-            offset=offset,
+            target_position=None,
+            target_orientation=None,
+            offset=None,
         )
+
         self._franka_prim_path = franka_prim_path
         self._franka_robot_name = franka_robot_name
         return
+    
+    def set_up_scene(self, scene: Scene) -> None:
+        super().set_up_scene(scene)
+        scene.add_default_ground_plane()
+
+        self._target = self.scene.get_object(self._target_name)
+        self._task_objects[self._target_name] = self._target
+        self._target_visual_material = self._target.get_applied_visual_material()
+
+        self._robot = self.set_robot()
+        self._task_objects[self._robot.name] = self._robot
+        self._move_task_objects_to_their_frame()
+
+    def set_params(self, *args, **kwargs):
+        return
 
     def set_robot(self) -> Franka:
-        """[summary]
-
-        Returns:
-            Franka: [description]
-        """
         if self._franka_prim_path is None:
-            self._franka_prim_path = find_unique_string_name(
-                initial_name="/World/Franka", is_unique_fn=lambda x: not is_prim_path_valid(x)
-            )
+            self._franka_prim_path = f"/World/Franka_{self.name}"  # e.g. /World/Franka_task0
         if self._franka_robot_name is None:
-            self._franka_robot_name = find_unique_string_name(
-                initial_name="my_franka", is_unique_fn=lambda x: not self.scene.object_exists(x)
-            )
+            self._franka_robot_name = f"my_franka_{self.name}"      # e.g. my_franka_task0
 
         robot = Franka(prim_path=self._franka_prim_path, name=self._franka_robot_name)
 
