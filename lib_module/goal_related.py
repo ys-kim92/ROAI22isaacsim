5998fde482fe95cf3b42f80a38b7c697bc066877
roai22 <ys.kim@roai.im>
2025-05-27 14:16:38 +0900

    [완료]goal related 코드 이동

--- lib_module/goal_related.py
+++ lib_module/goal_related.py
@@ -10,6 +10,9 @@ from isaacsim.core.utils.prims import create_prim
 from scipy.spatial.transform import Rotation as R
 import carb
 
+#+++++ Custom 모듈
+from isaacsim.examples.interactive.lib_module.data_io import DataIO
+
 
 
 class GoalRelated(RoaiBaseSample):
@@ -36,6 +39,65 @@ class GoalRelated(RoaiBaseSample):
         #carb.log_info(f"[FSM] Move to goal #{self._current_target_index} → pos: {pos}, rpy: {rpy}")
         print("--------------------------------------------------------")
         print(f"[FSM] Move to goal #{self._current_target_index} → pos: {pos}, rpy: {rpy}")
+
+    def _goal_Z_rotation(self):
+        # 회전 완료 조건
+        if self._current_Z_rotation_angle >= 360:
+            self._goal_rotation_done = True
+            print("Rotation complete. Moving to next target.")
+            return
+        
+        current_time = self._world.current_time
+        angle = self._current_Z_rotation_angle
+
+        # 로봇 제어를 위한 기본 정보 가져오기
+        robot = self._robots[self._current_robot_index]
+        controller = self._controllers[self._current_robot_index]
+        articulation_controller = self._articulation_controllers[self._current_robot_index]
+        observations = self._world.get_observations()
+        target_position = observations["SharedTarget"]["position"]
+        target_orientation = observations["SharedTarget"]["orientation"]
+        base_position, base_orientation = robot.get_world_pose()
+
+        # local_ori 보정 (이론상 그대로 가야 맞는데, 지금 franka hand 좌표계로 인해 y축 기준 180도 뒤집어야 함)
+        y_axis_rotation = euler_angles_to_quat(np.array([0, np.pi, 0]))
+        rotated_local_ori = quaternion_multiply(y_axis_rotation, target_orientation)
+
+        # 회전 각도 계산 및 상대좌표 설정
+        yaw_rotation = euler_angles_to_quat(np.array([0,0,np.radians(angle)]))
+        rotated_orientation = quaternion_multiply(rotated_local_ori,yaw_rotation)
+
+        local_pos, local_ori = GoalRelated._transform_goal_to_local_frame(
+            target_position, rotated_orientation, base_position, base_orientation
+        )
+
+        # 컨트롤러 동작
+        actions = controller.forward(
+            target_index=self._current_target_index,
+            target_end_effector_position=local_pos,
+            target_end_effector_orientation=local_ori,
+        )
+        kps, kds = self._tasks[self._current_robot_index].get_custom_gains()
+        articulation_controller.set_gains(kps, kds)
+        articulation_controller.apply_action(actions)
+
+        # 성공 여부 체크
+        if controller._success_flag:
+            self._any360_reached_flag = True
+            if (current_time - self._fsm_timer > self._goal_move_timeout):
+                DataIO._on_logging_event(self, angle)
+                print(f"Z rot angle: {angle} - Success")
+                self._current_Z_rotation_angle = angle + self._target_360_resolution
+
+                self._controllers[self._current_robot_index].reset()
+                self._reached_flag = False
+                self._fsm_timer = current_time
+        else:
+            print(f"Z rot angle: {angle} - Fail")
+            self._current_Z_rotation_angle = angle + self._target_360_resolution
+
+            # self._controllers[self._current_robot_index].reset()
+            self._fsm_timer = current_time
 
     @staticmethod
     def _visualize_every_goals(scene, filename="goals.json"):
@@ -96,3 +158,13 @@ def wxyz_to_xyzw(quat):
 @staticmethod
 def xyzw_to_wxyz(quat):
     return np.array([quat[3], quat[0], quat[1], quat[2]])
+
+@staticmethod
+def quaternion_multiply(q1, q2):
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
+    w = w1*w2 - x1*x2 - y1*y2 - z1*z2
+    x = w1*x2 + x1*w2 + y1*z2 - z1*y2
+    y = w1*y2 - x1*z2 + y1*w2 + z1*x2
+    z = w1*z2 + x1*y2 - y1*x2 + z1*w2
+    return np.array([w, x, y, z])
--- user_examples/roai_framework.py
+++ user_examples/roai_framework.py
@@ -202,7 +202,7 @@ class GoalValidation(RoaiBaseSample):
         elif self._planning_mode == 1:
             # base_position, base_orientation = robot.get_world_pose()  # quaternian wxyz 순서
             if not self._goal_rotation_done:
-                self.goal_Z_rotation()
+                GoalRelated._goal_Z_rotation(self)
 
         if self._goal_rotation_done:
             # shared target 위치 변경
@@ -237,71 +237,4 @@ class GoalValidation(RoaiBaseSample):
         self._world.stop()
         self._world.clear_all_callbacks()
         return
-    
-    def quaternion_multiply(self, q1, q2):
-        w1, x1, y1, z1 = q1
-        w2, x2, y2, z2 = q2
-        w = w1*w2 - x1*x2 - y1*y2 - z1*z2
-        x = w1*x2 + x1*w2 + y1*z2 - z1*y2
-        y = w1*y2 - x1*z2 + y1*w2 + z1*x2
-        z = w1*z2 + x1*y2 - y1*x2 + z1*w2
-        return np.array([w, x, y, z])
-    
-    def goal_Z_rotation(self):
-        # 회전 완료 조건
-        if self._current_Z_rotation_angle >= 360:
-            self._goal_rotation_done = True
-            print("Rotation complete. Moving to next target.")
-            return
         
-        current_time = self._world.current_time
-        angle = self._current_Z_rotation_angle
-
-        # 로봇 제어를 위한 기본 정보 가져오기
-        robot = self._robots[self._current_robot_index]
-        controller = self._controllers[self._current_robot_index]
-        articulation_controller = self._articulation_controllers[self._current_robot_index]
-        observations = self._world.get_observations()
-        target_position = observations["SharedTarget"]["position"]
-        target_orientation = observations["SharedTarget"]["orientation"]
-        base_position, base_orientation = robot.get_world_pose()
-
-        # local_ori 보정 (이론상 그대로 가야 맞는데, 지금 franka hand 좌표계로 인해 y축 기준 180도 뒤집어야 함)
-        y_axis_rotation = euler_angles_to_quat(np.array([0, np.pi, 0]))
-        rotated_local_ori = self.quaternion_multiply(y_axis_rotation, target_orientation)
-
-        # 회전 각도 계산 
-        yaw_rotation = euler_angles_to_quat(np.array([0,0,np.radians(angle)]))
-        rotated_orientation = self.quaternion_multiply(rotated_local_ori,yaw_rotation)
-
-        local_pos, local_ori = GoalRelated._transform_goal_to_local_frame(
-            target_position, rotated_orientation, base_position, base_orientation
-        )
-
-        # 컨트롤러 동작
-        actions = controller.forward(
-            target_index=self._current_target_index,
-            target_end_effector_position=local_pos,
-            target_end_effector_orientation=local_ori,
-        )
-        kps, kds = self._tasks[self._current_robot_index].get_custom_gains()
-        articulation_controller.set_gains(kps, kds)
-        articulation_controller.apply_action(actions)
-
-        # 성공 여부 체크
-        if controller._success_flag:
-            self._any360_reached_flag = True
-            if (current_time - self._fsm_timer > self._goal_move_timeout):
-                DataIO._on_logging_event(self, angle)
-                print(f"Z rot angle: {angle} - Success")
-                self._current_Z_rotation_angle = angle + self._target_360_resolution
-
-                self._controllers[self._current_robot_index].reset()
-                self._reached_flag = False
-                self._fsm_timer = current_time
-        else:
-            print(f"Z rot angle: {angle} - Fail")
-            self._current_Z_rotation_angle = angle + self._target_360_resolution
-
-            # self._controllers[self._current_robot_index].reset()
-            self._fsm_timer = current_time
